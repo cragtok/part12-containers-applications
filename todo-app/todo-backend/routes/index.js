2367e158321bec9dff2cf73a042f7a90a50e842f
cragtok <lidfun@protonmail.com>
2024-11-15 18:51:10 +0600

    exercise 12.10

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,19 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const redis = require('../redis')
+const { getAsync } = require("../redis");
 
-const configs = require('../util/config')
+const configs = require("../util/config");
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
-router.get('/', async (req, res) => {
-  visits++
+router.get("/", async (req, res) => {
+    visits++;
 
-  res.send({
-    ...configs,
-    visits
-  });
+    res.send({
+        ...configs,
+        visits,
+    });
+});
+
+router.get("/statistics", async (req, res) => {
+    const addedTodos = await getAsync("addedTodos");
+
+    res.json({ added_todos: addedTodos === null ? 0 : Number(addedTodos) });
 });
 
 module.exports = router;
